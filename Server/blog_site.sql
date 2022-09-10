9d0fbd97361fe735871afac3a85397d3aa8f3b4f
xavierbrasher <xavier.brasher@gmail.com>
2022-09-10 14:39:11 +0800

    added postgresql template file

--- Server/blog_site.sql
+++ Server/blog_site.sql
@@ -1,2 +1,2 @@
-insert into "blogs" ("author", "content", "datecreated", "id", "title", "user") values ('Xavier Brasher', 'Wow this is test data :)', '2022-09-10', 1, 'Wow such a cool blog', 'Xavier');
-insert into "blogs" ("author", "content", "datecreated", "id", "title", "user") values ('Ezra Filler', 'Wow this is test data :)', '2022-09-10', 2, 'Im shitty', 'Ezra');
+insert into "blogs" ("author", "content", "datecreated", "id", "title", "user") values ('Xavier B', 'Wow this is test data :)', '2022-09-10', 1, 'Wow such a cool blog', 'Xavier');
+insert into "blogs" ("author", "content", "datecreated", "id", "title", "user") values ('Ezra F', 'Wow this is test data :)', '2022-09-10', 2, 'Im shitty', 'Ezra');
