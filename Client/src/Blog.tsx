80c91aebb296ba48e0f1204169885219c6bc99c4
xavierbrasher <xavier.brasher@gmail.com>
2022-09-14 21:09:39 +0800

    Added individual blog screen

--- Client/src/Blog.tsx
+++ Client/src/Blog.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { BlogStruct, dateFormat, titleStruct } from "./lib/blog-struct";
 import useBlogs from "./lib/useBlogs";
@@ -19,7 +20,7 @@ const Title = ({ title, dateCreated }: titleStruct) => {
 const contentStucture = (content: String) => {
     let tmp = "";
     for (let i = 0; i < content.length; i++) {
-        if (i % 100 === 0) {
+        if (i % 120 === 0 && i !== 0) {
             tmp += "\n";
         }
         tmp += content[i];
@@ -29,26 +30,45 @@ const contentStucture = (content: String) => {
 
 export default function Blog() {
     let { id } = useParams();
-    let blog: BlogStruct = useBlogs.getBlog(parseInt(id!));
+    let [blog, setBlock] = useState<BlogStruct>();
+    let [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function getBlog() {
+            return await useBlogs.getBlog(parseInt(id || "1"));
+        }
+        getBlog().then((value) => {
+            setBlock(value);
+            setLoading(false);
+        });
+    }, [id]);
 
     return (
-        <div className="flex justify-center mb-4 mt-4">
-            <div className="card border-2 border-dotted border-neutral shadow">
-                <Title title={blog.title} dateCreated={blog.dateCreated} />
-                <p className="mx-4">By {blog.author}</p>
-                <p className="mt-1 mx-4">
-                    {contentStucture(blog.content).map((content, idx) => {
-                        if (content === "") {
-                            return <div key={idx}></div>;
-                        }
-                        return (
-                            <div key={idx}>
-                                {content} <br />
-                            </div>
-                        );
-                    })}
-                </p>
-            </div>
+        <div>
+            {!loading && blog ? (
+                <div className="flex justify-center mb-4 mt-4">
+                    <div className="card border-2 border-dotted border-neutral shadow">
+                        <Title
+                            title={blog.title}
+                            dateCreated={blog.dateCreated}
+                        />
+                        <p className="mx-4">By {blog.author}</p>
+                        <p className="mt-1 mx-4 break-before-avoid">
+                            {contentStucture(blog.content).map((content) => {
+                                return (
+                                    <>
+                                        {content} <br />
+                                    </>
+                                );
+                            })}
+                        </p>
+                    </div>
+                </div>
+            ) : (
+                <h2 className="text-center text-5xl font-bold mb-6">
+                    Loading ...
+                </h2>
+            )}
         </div>
     );
 }
--- Client/src/lib/useBlogs.ts
+++ Client/src/lib/useBlogs.ts
@@ -1,16 +1,12 @@
 import { BlogStruct } from "./blog-struct";
 export default class useBlogs {
-    static getBlog(blogId: number) {
-        let x: BlogStruct = {
-            id: 69,
-            author: "Xavier",
-            content:
-                "WWWow this is crazy i am so madWow this is crazy i am so madWow this is crazy i am so madWow this is crazy i am so madWow this is crazy i am so madWow this is crazy i am so madWow this is crazy i am so madWow this is crazy i am so madWow this is crazy i am so madWow this is crazy i am so madWow this is crazy i am so madWow this is crazy i am so madWow this is crazy i am so madWow this is crazy i am so madWow this is crazy i am so madWow this is crazy i am so madWow this is crazy i am so madow this is crazy i am so mad Wow this is crazy i am so mad Wow this is crazy i am so madWow this is crazy i am so mad Wow this is crazy i am so mad Wow this is crazy i am so madWow this is crazy i am so mad Wow this is crazy i am so mad Wow this is crazy i am so madWow this is crazy i am so mad Wow this is crazy i am so mad Wow this is crazy i am so madWow this is crazy i am so mad Wow this is crazy i am so mad Wow this is crazy i am so madWow this is crazy i am so mad Wow this is crazy i am so mad Wow this is crazy i am so madWow this is crazy i am so mad Wow this is crazy i am so mad Wow this is crazy i am so madWow this is crazy i am so mad Wow this is crazy i am so mad Wow this is crazy i am so madow this is crazy i am so mad Wow this is crazy i am so mad Wow this is crazy i am so mad",
-            dateCreated: new Date("August 19, 1975 23:15:30"),
-            user: "XavXavier",
-            title: "This is so cool and its a test",
-        };
-        return x;
+    static async getBlog(blogId: number) {
+        // const apiURL = `http://${window.location.host}/api/blogs`; //TODO: Get rid of debug in production
+        const apiURL = `http://${window.location.hostname}:8080/api/blog/${blogId}`;
+        const responce = await fetch(apiURL);
+        const res = await responce.json();
+        const blog: BlogStruct = await this.formatApiResponce(res);
+        return blog;
     }
 
     static formatApiResponce(notFormatedArray: any) {
